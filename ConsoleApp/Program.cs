71eeef0b21dcfe3667052d05ad1973338766bee2
Alexis Bonneau <60471569+alex892-droid@users.noreply.github.com>
2023-07-04 21:50:28 +0200

    Correction ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,11 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Cryptography;
 
-CryptographyService cryptographyService = new CryptographyService();
-
-var keyPair = cryptographyService.GenerateKeyPair();
+var keyPair = CryptographyService.GenerateKeyPair();
 var message = "test";
-var signature = cryptographyService.Sign(message, keyPair.PrivateKey);
-var isVerified = cryptographyService.VerifySignature(message, signature, keyPair.PublicKey);
+var signature = CryptographyService.Sign(message, keyPair.PrivateKey);
+var isVerified = CryptographyService.VerifySignature(message, signature, keyPair.PublicKey);
 
 Console.WriteLine(isVerified);
